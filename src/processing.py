a507bdc952b85ab61ba20b5ad806685a54b00ca7
Your Name <you@example.com>
2024-06-22 18:22:15 +0300

    fixes in processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-original_list = [
+orig_list = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -6,7 +6,7 @@ original_list = [
 ]
 
 
-def filter_by_state(original_list: list[str], state_id = "EXECUTED") -> list[dict[str]]:
+def filter_by_state(original_list: list[str], state_id: str = "EXECUTED") -> list[dict[str]]:
     """Функция, принимающая список словарей и выдает новый список с заданным ключом"""
 
     new_list = []
@@ -15,3 +15,16 @@ def filter_by_state(original_list: list[str], state_id = "EXECUTED") -> list[dic
         if key.get("state") == state_id:
             new_list.append(key)
     return new_list
+
+
+#print(filter_by_state(orig_list, "CANCELED"))
+
+
+def sort_by_date(inform_state: list[dict[str]], reverse=True) -> list[dict[str]]:
+    """Функиця, сортирующая исходные данные по дате"""
+
+    sorted_inform_state = sorted(inform_state, key=lambda inform_state: inform_state["date"], reverse=reverse)
+    return sorted_inform_state
+
+
+#print(sort_by_date(orig_list, True))
