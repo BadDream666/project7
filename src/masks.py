02cf1acb0d7d5f98296e642ff0da668874a4ccbc
Your Name <you@example.com>
2024-06-24 18:39:29 +0300

    исправление требований flake8 и mypy

--- src/masks.py
+++ src/masks.py
@@ -6,15 +6,9 @@ def get_mask_card_number(card_number: str) -> str | None:
         return None
 
 
-# print(get_mask_card_number("7158300734726758"))
-
-
 def get_mask_account(acc_number: str) -> str | None:
     """Returns masked account number as string"""
     if acc_number.isdigit() and len(acc_number) == 20:
         return f"**{acc_number[-4::]}"
     else:
         return None
-
-
-# print(get_mask_account("73654108430135874305"))
--- src/processing.py
+++ src/processing.py
@@ -17,14 +17,22 @@ def filter_by_state(original_list: list[dict], state: str = "EXECUTED") -> list[
     return filtered_list
 
 
-# print(filter_by_state(original_list, "CANCELED"))
-# print(filter_by_state(original_list, "EXECUTED"))
-
-def sort_by_date(original_list: list[dict], reverse=True[dict]) -> list[dict]:
+def sort_by_date(original_list: list[dict], reverse: bool = True) -> list[dict]:
     """Функиця, сортирующая исходные данные по дате"""
 
     sorted_list: list[dict] = sorted(original_list, key=lambda inform_state: inform_state["date"],
-                                     reverse=reverse[dict])
+                                     reverse=reverse)
     return sorted_list
 
-# print(sort_by_date(original_list, True))
+
+if __name__ == '__main__':
+    original_list = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+    print(filter_by_state(original_list, "CANCELED"))
+    print(filter_by_state(original_list, "EXECUTED"))
+
+    print(sort_by_date(original_list, True))
