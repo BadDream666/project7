5368e1b4ba9cbb7e77f4608c913896646b9afc9c
Your Name <you@example.com>
2024-07-14 16:54:49 +0300

    12.1 homework

---
+++ tests/test_external_api.py
@@ -0,0 +1,10 @@
+from unittest.mock import patch
+from src.external_api import convert_from_usd_to_rub
+
+
+@patch('requests.get')
+def test_convert_from_usd_to_rub(mock_get):
+    mock_get.return_value.json.return_value = '{"result": 60}'
+    assert convert_from_usd_to_rub(20, "USD") == 60
+    mock_get.assert_called_once_with("https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=20",
+                                     headers={'apikey': 'oN80ujwnohbxx2OoUKUFY6uU8XcuRp9b'})
